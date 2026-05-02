ab7399d63ce516ba30a3179cbd57cd7d2aa9899a
SHRP <alex@sherpa.one>
2026-05-02 21:22:22 +0200

    variables d'environnement

--- src/index.ts
+++ src/index.ts
@@ -1,31 +1,6 @@
-import { Hono } from "hono";
-import { planets } from "./planets";
-import { toSummary } from "./utils";
+import { app } from "./app";
 
-const app = new Hono();
-
-app.get("/planets", (c) => {
-  return c.json(
-    {
-      data: planets.map(toSummary),
-      message: "List of the Solar System 8 planets with summary information",
-    },
-    200,
-  );
-});
-
-app.get("/planets/:id", (c) => {
-  const planet = planets.find((p) => p.id === c.req.param("id"));
-  if (!planet) {
-    return c.json({ error: "Planet not found" }, 404);
-  }
-  return c.json(
-    {
-      data: planet,
-      message: `Detailed information about planet ${planet.name}`,
-    },
-    200,
-  );
-});
-
-export default app;
+export default {
+  port: Bun.env.PORT,
+  fetch: app.fetch,
+};
